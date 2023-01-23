b29636990774210dcab08927cc7169da6393367c
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-01-23 22:49:23 +0600

    Create tag question.py

---
+++ tag question.py
@@ -0,0 +1,36 @@
+import nltk
+from nltk import word_tokenize
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+auxiliary_verbs=["am","is","are","was","were","have","has","had"]
+
+def get_verb_from_sent(sent):
+    tokens = nltk.word_tokenize(sent.lower().replace("'m", " am"))
+    pos_tags = nltk.pos_tag(tokens)
+    print("pos_tags", pos_tags)
+    for word, pos in pos_tags:
+        if pos == "MD":
+            return word
+        elif pos == "VBP":
+            if word in auxiliary_verbs:
+                return word
+            else:
+                return "do"
+        elif pos == "VBZ":
+            if word in auxiliary_verbs:
+                return word
+            else:
+                return "does"
+        elif pos == "VBD":
+            if word in auxiliary_verbs:
+                return word
+            else:
+                return "did"
+    return False
+
+while True:
+    sentence = input("Enter sentence: ")
+    print("Verb:", get_verb_from_sent(sentence))
+
