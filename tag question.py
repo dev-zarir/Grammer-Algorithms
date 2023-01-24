643090a79ce5f2f28161ab3f316b24f035cd5a24
Your Name <you@example.com>
2023-01-24 17:47:49 +0600

    New Commit

--- tag question.py
+++ tag question.py
@@ -1,24 +1,72 @@
 import spacy
 
-# Load the en_core_web_sm model
 nlp = spacy.load("en_core_web_sm")
+operators=['am', 'is', 'are', 'was', 'were', 'have', 'has', 'had', 'can', 'could', 'shall', 'should', 'will', 'would', 'may', 'might', 'must', 'dare', 'need', 'used', 'ought']
+exceptional_neg_verbs = {'am': "aren't", 'can': "can't", 'shall': "shan't", 'will': "won't"}
 
-# Define the sentence
-sentence = "Rahim who is a youtuber wants to get a job"
+def get_dep_pos_tag(sent:str) -> list:
+    doc = nlp(sent)
+    data=[]
+    for token in doc:
+        data.append((token.text, token.dep_, token.pos_, token.tag_))
+    return data
 
-# Parse the sentence
-doc = nlp(sentence)
+def get_negative_verb(verb:str) -> list:
+    if verb in exceptional_neg_verbs:
+        return exceptional_neg_verbs.get(verb)
+    else:
+        return verb + "n't"
 
-# Extract the principal clause by iterating over the subtree of the root token
-principal_clause = []
-for token in doc:
-    if token.dep_ == "ROOT":
-        for t in token.subtree:
-            principal_clause.append(t.text)
-        break
+def get_if_negative_sent(sent:str) -> bool:
+    doc = nlp(sent)
+    for token in doc:
+        if token.dep_ == 'neg':
+            return True
+    return False
 
-# Join the words of the principal clause to form a sentence
-principal_clause = " ".join(principal_clause)
+def get_verb_from_sent(sent:str) -> list:
+    doc = nlp(sent)
+    verbs=[]
+    for token in doc:
+        if token.dep_ == 'ROOT':
+            if token.tag_ == 'VBP': verbs.append('do')
+            if token.tag_ == 'VBD': verbs.append('did')
+            if token.tag_ == 'VBZ': verbs.append('does')
+    return verbs
 
-# Print the principal clause
-print(principal_clause)
+def get_aux_verb_from_sent(sent:str) -> list:
+    doc = nlp(sent)
+    aux_verbs=[]
+    for token in doc:
+        if token.pos_ == 'AUX': aux_verbs.append(token.text)
+    return aux_verbs
+
+def get_subject_from_sent(sent:str) -> list:
+    doc = nlp(sent)
+    subjects=[]
+    for token in doc:
+        # if token.dep_ == 'nsubj': subjects.append(token.text)
+        if token.dep_ == 'nsubj': subjects.append(' '.join([t.text for t in list(token.subtree)]))
+    return subjects
+
+def solve_tag_question(sent:str) -> str:
+    sentence = sent
+    subjects=get_subject_from_sent(sentence)
+    aux_verbs=get_aux_verb_from_sent(sentence)
+    main_verbs=get_verb_from_sent(sentence)
+    negative=get_if_negative_sent(sentence)
+    print(get_dep_pos_tag(sentence))
+    print('Subjects:', subjects)
+    print('Auxilary Verbs:', aux_verbs)
+    print('Main Verbs:', main_verbs)
+    print('Negative:', negative)
+    if aux_verbs: verb=aux_verbs[0]
+    else: verb=main_verbs[0]
+    if not negative: verb=get_negative_verb(verb)
+    return f'{verb} {subjects[0]}'
+
+while 1:
+    str_in = input('Sentence: ')
+    if str_in == 'exit': quit()
+    print('Answer:', solve_tag_question(str_in))
+
