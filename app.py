f283d35739a74819964f19fe640a1f0c952a828a
Zarir <shahriarzariradvance@gmail.com>
2023-01-27 16:55:21 +0600

    New commit

--- app.py
+++ app.py
@@ -22,5 +22,29 @@ def tag_solver_api():
         return jsonify({'success':False, 'question':question})
     return jsonify({'success':True, 'question':question, 'answer': answer})
 
+@app.route('/debug/<sent>', methods=['GET', 'POST'])
+def tag_debug(sent:str):
+    global text
+    text=""
+    def print(*args):
+        global text
+        text+=" ".join(*args)+"\n"
+    # SUBTREE
+    print('SUBTREE START')
+    doc = nlp(sent)
+    for token in doc:
+        print(token.text + ',', ' '.join([t.text for t in list(token.subtree)]) + ',', token.morph)
+    print('SUBTREE END')
+    # EXPLAIN
+    print("EXPLAIN START")
+    doc = nlp(sent)
+    data=[]
+    for token in doc:
+        data.append((token.text, token.dep_, token.pos_, token.tag_))
+    print(data)
+    print("EXPLAIN END")
+    
+    return text
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
